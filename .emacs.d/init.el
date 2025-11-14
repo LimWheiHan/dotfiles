92e86b9b5e17d46171ea974d6f68c98ed6dba42d
LimWheiHan <han9.2000@gmail.com>
2025-11-14 23:39:09 +0800

    perf(emacs/init): tweak gc in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,21 +2,33 @@
 
 (require 'package)
 
-;;;Code:
-(setq package-enable-at-startup nil)
+;; (setq package-enable-at-startup nil)
 
 
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(unless (assoc-default "melpa" package-archives)
-  (add-to-list 'package-archives
-	       '("melpa" . "https://melpa.org/packages/") t))
+(setq package-archives
+	  '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+		("MELPA Stable" . "https://stable.melpa.org/packages/")
+		("ORG"          . "https://orgmode.org/elpa/")
+		("MELPA"        . "https://melpa.org/packages/")
+		("nongnu"       . "https://elpa.nongnu.org/nongnu/"))
+	  package-archive-priorities
+	  '(("GNU ELPA"     . 20)
+		("MELPA Stable" . 15)
+		("ORG"          . 10)
+		("MELPA"        . 5)
+		("nongnu"       . 0)))
 
-(unless (assoc-default "nongnu" package-archives)
-  (add-to-list 'package-archives
-	       '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
+;; (unless (assoc-default "melpa" package-archives)
+;;   (add-to-list 'package-archives
+;; 	       '("melpa" . "https://melpa.org/packages/") t))
+
+;; (unless (assoc-default "nongnu" package-archives)
+;;   (add-to-list 'package-archives
+;; 	       '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
 
 
 ;; (add-to-list 'package-archives
@@ -30,19 +42,56 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
+;; Don't follow to the dotfile directory
+(setq vc-follow-symlinks nil)
 
 ;;Set UTF-8 encoding
+;; UTF-8 as default encoding
 (setq locale-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+(setq use-package-always-ensure t)
 
 ; Store emacs generated customisation here
 (setq custom-file "~/.emacs.d/custom.el")
 (load-file custom-file)
+
+;; Improved Garbage Collection
+;; FROM EMACS FROM SCRATCH
+;; Make gc pauses faster by decreasing the threshold.
+;; (setq gc-cons-threshold (* 2 1000 1000))
+
+;; FROM UNCLE DAVE
+;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
+;; Automatically Done in Doom?
+;; ref: github.com/doomemacs/doomemacs/issues/3108
+;; (after! gcmh
+;;   (setq gc-cons-threshold  33554432
+;;       gc-cons-percentage 0.6))  ; 32mb, or 64mb, or *maybe* 128mb, BUT NOT 512mb
 
 ;; Use Configuration from config.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
